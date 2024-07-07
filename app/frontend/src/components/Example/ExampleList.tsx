072879d2b7d394adb5e0e43e849e32bd2b94703f
Kyriakos Antoniadis <contact@ai-systems.today>
2024-07-07 12:29:33 +0000

    customized the conversation starters

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "Can you provide a summary of the main objectives for the HEDNO Data Cleansing Project?",
+    "What are the key methodologies and technologies used by Incelligent in the HEDNO?",
+    "List the roles and responsibilities of Incelligent, Grant Thornton and WEMETRIX?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
